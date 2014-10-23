4f23977d6c482be4dd318d2e51ceeefa535eb749
Alex Baker <al3xb4k3r@gmail.com>
2014-10-23 00:20:28 -0600

    added admin console to manage blog entries, styled blog entries, added date to db schema

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os
-import logging
+from datetime import date
 
-from flask import Flask, request, session, g, redirect, url_for, abort, render_template
+from flask import Flask, request, session, g, redirect, url_for, render_template
 from sqlite3 import dbapi2 as sqlite3
 
 import secrets
@@ -20,9 +20,13 @@ app.config.update(dict(
 ))
 app.config.from_envvar('MAIN_SETTINGS', silent=True)
 
+
+#############################################################
+######              Database Functions                 ######
+#############################################################
 def connect_db():
     """Connects to the specific database."""
-    rv = sqlite3.connect(app.config['DATABASE'])
+    rv = sqlite3.connect(app.config['DATABASE'], detect_types=sqlite3.PARSE_DECLTYPES)
     rv.row_factory = sqlite3.Row
     return rv
 
@@ -53,6 +57,10 @@ def close_db(error):
     if hasattr(g, 'sqlite_db'):
         g.sqlite_db.close()
 
+
+#############################################################
+######                 Public Routes                   ######
+#############################################################
 @app.route('/')
 def front():
     return render_template('front.html')
@@ -60,18 +68,32 @@ def front():
 @app.route('/blog')
 def blog():
     db = get_db()
-    cur = db.execute('select title, text from entries order by id desc')
-    entries = [dict(title=row[0], text=row[1]) for row in cur.fetchall()]
+    cur = db.execute('select date, title, text from entries order by id desc')
+    entries = [dict(date=row[0].strftime('%B %d, %Y'), title=row[1], text=row[2]) for row in cur.fetchall()]
     return render_template('blog.html', entries=entries)
 
-@app.route('/blog/add', methods=['GET', 'POST'])
+
+#############################################################
+######                 Admin Routes                    ######
+#############################################################
+@app.route('/admin')
+def admin():
+    if session.get('logged_in'):
+        db = get_db()
+        cur = db.execute('select id, date, title from entries order by id desc')
+        entries = [dict(id=row[0], date=row[1], title=row[2]) for row in cur.fetchall()]
+        return render_template('admin.html', entries=entries)
+    else:
+        return redirect(url_for('login'))
+
+@app.route('/admin/add', methods=['GET', 'POST'])
 def add_entry():
     if session.get('logged_in'):
         if request.method == 'POST':
             db = get_db()
             db.execute(
-                'insert into entries (title, text) values (?, ?)',
-                [request.form['title'], request.form['text']]
+                'insert into entries (date, title, text) values (?, ?, ?)',
+                [date.today(), request.form['title'], request.form['text']]
             )
             db.commit()
             return redirect(url_for('blog'))
@@ -79,8 +101,40 @@ def add_entry():
     else:
         return redirect(url_for('login'))
 
-@app.route('/blog/login', methods=['GET', 'POST'])
+@app.route('/admin/edit/<entry_id>', methods=['GET', 'POST'])
+def edit_entry(entry_id):
+    if session.get('logged_in'):
+        if request.method == 'POST':
+            db = get_db()
+            db.execute(
+                'update entries set title = ?, text = ? where id = ?', [request.form['title'], request.form['text'], entry_id]
+            )
+            db.commit()
+            return redirect(url_for('admin'))
+        db = get_db()
+        cur = db.execute(
+            'select title, text from entries where id = ?', [entry_id]
+        ).fetchone()
+        return render_template('add_entry.html', entry_id=entry_id, title=cur[0], content=cur[1])
+    else:
+        return redirect(url_for('login'))
+
+@app.route('/admin/delete/<entry_id>')
+def delete_entry(entry_id):
+    if session.get('logged_in'):
+        db = get_db()
+        db.execute(
+            'delete from entries where id=%s' % entry_id
+        )
+        db.commit()
+        return redirect(url_for('admin'))
+    else:
+        return redirect(url_for('login'))
+
+@app.route('/admin/login', methods=['GET', 'POST'])
 def login():
+    if session.get('logged_in'):
+        return redirect(url_for('add_entry'))
     error = None
     if request.method == 'POST':
         if request.form['username'] != app.config['USERNAME']:
@@ -89,10 +143,10 @@ def login():
             error = "Invalid password"
         else:
             session['logged_in'] = True
-            return redirect(url_for('add_entry'))
+            return redirect(url_for('admin'))
     return render_template('login.html', error=error)
 
-@app.route('/blog/logout')
+@app.route('/admin/logout')
 def logout():
     session.pop('logged_in', None)
     return redirect(url_for('blog'))
