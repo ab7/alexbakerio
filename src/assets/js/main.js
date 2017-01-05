c908327acc7ae4b627c608b5e27d38c9fda598e3
Alex Baker <al3xb4k3r@gmail.com>
2017-01-05 00:01:03 -0700

    Removed unused images and js.

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -2,31 +2,6 @@
 
 $(function () {
   'use strict';
-  var $window, $navBar, $stick, windowTop, navTop, introTop, $topLink;
-
-  // // sticky nav on scroll
-  // $window = $(window);
-  // $navBar = $('nav');
-  // $stick = $('.stick');
-  // $topLink = $('.nav-top-link');
-
-  // $window.scroll(function () {
-  //   windowTop = $window.scrollTop();
-  //   navTop = $navBar.offset().top;
-  //   introTop = $stick.offset().top;
-
-  //   if (windowTop >= navTop) {
-  //     $navBar.addClass('nav-sticky');
-  //     if ($window.width() >= 440) {
-  //       $topLink.fadeIn();
-  //     }
-  //   }
-
-  //   if ((introTop - $navBar.height()) >= windowTop) {
-  //     $navBar.removeClass('nav-sticky');
-  //     $topLink.hide();
-  //   }
-  // });
 
   // Mobile menu
   var options = {
